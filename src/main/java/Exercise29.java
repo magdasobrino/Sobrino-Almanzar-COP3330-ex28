ce2cb281272c676f8bd77f345f1202ecb230e9f1
magdasobrino <magdalena.sobrino@knights.ucf.edu>
2021-09-20 23:49:22 -0400

    Initial commit

---
+++ src/main/java/Exercise29.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Magdalena Sobrino-Almanzar
+ */
+import java.util.Scanner;
+
+public class Exercise29 {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("What is the rate of return?");
+        input = scnr.nextInt();
+
+        System.out.println("What is the rate of return?");
+        input = scnr.nextLine();
+
+        System.out.println("What is the rate of return?");
+        input = scnr.nextLine();
+
+
+
+    }
+}
